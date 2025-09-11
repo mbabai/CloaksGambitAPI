9ae98953caa07691b63493011c1e9e8018334694
Marcell Babai <marcellbabai@gmail.com>
2025-09-11 16:07:28 -0700

    ***** MEGA REFACTOR: remove "frontend" which was a dummy folder.

--- frontend/src/app/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import GameUI from '../features/play/ui/GameUI.jsx'
-import '../shared/styles/theme.css'
-
-export default function App() {
-  return <GameUI />
-}
-
--- frontend/src/shared/ui/Button.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import styles from './Button.module.css'
-
-export default function Button({ className = '', style = {}, ...props }) {
-  return <button className={`${styles.button} ${className}`} style={style} {...props} />
-}
